7faf821f8d0e21214ffdcfb87f570a985fe1639c
Adrian <adrian@mxlinux.org>
2017-03-30 09:46:42 -0400

    build with new translations

--- translations/mx-packageinstaller_lt.ts
+++ translations/mx-packageinstaller_lt.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
 <context>
     <name>QApplication</name>
     <message>
