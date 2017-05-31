891f2a791d0715c0e086f45f3ae8502326347028
oldgizahub <geoffgigg@hotmail.com>
2017-05-31 12:08:44 -0400

    revised translations for mx-packageinstaller: ja

--- translations/mx-packageinstaller_ja.ts
+++ translations/mx-packageinstaller_ja.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="ja">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ja" version="2.1">
 <context>
     <name>MainWindow</name>
     <message>
@@ -15,7 +13,7 @@
     <message>
         <location filename="../mainwindow.ui" line="42"/>
         <source>Popular Applications</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="68"/>
@@ -45,65 +43,65 @@
     <message>
         <location filename="../mainwindow.ui" line="108"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:16pt;&quot;&gt;Manage popular packages&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Greyed out items have already been installed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="138"/>
         <source>Full App Catalog</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="144"/>
         <source>Select source</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="150"/>
         <location filename="../mainwindow.cpp" line="1264"/>
         <source>Stable Repo</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="160"/>
         <location filename="../mainwindow.cpp" line="1263"/>
         <source>MX Test Repo</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="170"/>
         <location filename="../mainwindow.cpp" line="1262"/>
         <source>Debian Backports Repo</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="258"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Filter packages according to their status.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="261"/>
         <location filename="../mainwindow.ui" line="265"/>
         <location filename="../mainwindow.cpp" line="1304"/>
         <source>All packages</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="270"/>
         <location filename="../mainwindow.cpp" line="1317"/>
         <source>Installed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="275"/>
         <location filename="../mainwindow.cpp" line="1315"/>
         <source>Upgradable</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="280"/>
         <location filename="../mainwindow.cpp" line="1319"/>
         <source>Not installed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="288"/>
@@ -113,7 +111,7 @@
     <message>
         <location filename="../mainwindow.ui" line="313"/>
         <source>Refresh list</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="353"/>
@@ -128,42 +126,42 @@
     <message>
         <location filename="../mainwindow.ui" line="373"/>
         <source>Displayed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="381"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;= Upgradable package. Newer version available in selected repository.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="403"/>
         <source>Total packages:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="433"/>
         <source>Installed:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="463"/>
         <source>Upgradable:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="477"/>
         <source>Upgrade All</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="502"/>
         <source>Hide library and developer packages</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="545"/>
         <source>Uninstall</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="579"/>
@@ -220,7 +218,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="96"/>
         <source>Uninstalling packages...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="113"/>
@@ -237,12 +235,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="292"/>
         <source>Please wait...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="425"/>
         <source>Updating package list...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="461"/>
@@ -302,12 +300,12 @@
         <location filename="../mainwindow.cpp" line="649"/>
         <location filename="../mainwindow.cpp" line="748"/>
         <source>Internet is not available, won&apos;t be able to download the list of packages</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="567"/>
         <source>Installing packages...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="596"/>
@@ -317,12 +315,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="601"/>
         <source>Post-processing...</source>
-        <translation type="unfinished"></translation>
+        <translation>後処理中...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="624"/>
         <source>Pre-processing for </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="630"/>
@@ -332,7 +330,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="635"/>
         <source>Post-processing for </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="684"/>
@@ -342,7 +340,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="685"/>
         <source>Process finished.&lt;p&gt;&lt;b&gt;Do you want to exit MX Package Installer?&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="686"/>
@@ -357,12 +355,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="756"/>
         <source>Downloading package info...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="834"/>
         <source>Reading downloaded file...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1034"/>
@@ -372,7 +370,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="1071"/>
         <source>Package info</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1162"/>
@@ -387,7 +385,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="1164"/>
         <source>Package Installer for MX Linux</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1166"/>
@@ -403,22 +401,22 @@
     <message>
         <location filename="../mainwindow.cpp" line="1209"/>
         <source>Reinstall</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1260"/>
         <source>Repo Selection</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1261"/>
         <source>Please select repo to load</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1360"/>
         <source>Upgrade</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
@@ -426,17 +424,17 @@
     <message>
         <location filename="../main.cpp" line="56"/>
         <source>Unable to get exclusive lock</source>
-        <translation type="unfinished"></translation>
+        <translation>排他ロックを取得できません</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="57"/>
         <source>Another package management application (like Synaptic or apt-get), is already running. Please close that application first</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../main.cpp" line="69"/>
         <source>You must run this program as root.</source>
-        <translation type="unfinished"></translation>
+        <translation>このプログラムは root で実行する必要があります。</translation>
     </message>
 </context>
 </TS>
