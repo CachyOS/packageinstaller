13ce22dcc972473f041a652f399d4e7bd32a85ce
Adrian <adrian@mxlinux.org>
2021-01-04 11:33:25 -0500

    add modification notice

--- versionnumber.cpp
+++ versionnumber.cpp
@@ -1,5 +1,6 @@
 /*
     Copyright (C) 2008  Tim Fechtner < urwald at users dot sourceforge dot net >
+    Modfied by Adrian @MXLinux
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
